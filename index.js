aba13fa85e9d97aa90e6769c15ae0db9f4e6339a
alisabaj <alisabaj@gmail.com>
2019-11-26 09:50:26 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData (name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            appendToDom(object)
+        })
+        .catch(function(error) {
+            document.body.textContent = "Unauthorized Access"
+        })
+}
+
+function appendToDom(object) {
+    let body = document.querySelector('body')
+    let p = document.createElement('p')
+    p.textContent = object.id 
+    body.appendChild(p)
+}
